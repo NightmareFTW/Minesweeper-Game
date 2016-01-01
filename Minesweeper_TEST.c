57ebb1ee0e043e1550d5696807d431245a872e9f
Hector Smith <hector.j.smith@outlook.com>
2016-01-01 22:59:41 +0000

    Pequenas melhorias a aparencia do codigo
    Ajuste feito para que quando todas as casas sem bombas forem abertas, o jogador ganha
    Melhorias feitas a aparencia do codigo para tentar aumentar a clareza do codigo

--- Minesweeper_TEST.c
+++ Minesweeper_TEST.c
@@ -3,21 +3,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-const int Side = 10; // Comprimento dos lados do jogo
+// Constantes
+const int SIDE = 10; // Comprimento dos lados do jogo
 const int NUMBOMBS = 10; // Número de bombas
 const double DUMMY = 0.001; // Tolerância, para que as bombas adjacentes sejam contadas devidamente.
 
+// Funções
 int playgame();
 char getoption();
-void printboard(char board[][Side]);
+void printboard(char board[][SIDE]);
 int numbombs(int row,int column,int listbombs[][2]);
-int domark(char board[][Side], int listbombs[][2], int row, int column);
-int domove(char board[][Side], char realboard[][Side], int listbombs[][2], int row, int column, int *totalmoves);
+int domark(char board[][SIDE], int listbombs[][2], int row, int column);
+int domove(char board[][SIDE], char realboard[][SIDE], int listbombs[][2], int row, int column, int *totalmoves);
 void getbombs(int bomblist[][2]);
 void getmove(int *x, int *y);
 int valid(int row,int column);
 
-
+// Inicio da applicação
 int main() {
   
     int score = 0;
@@ -35,29 +37,29 @@ int main() {
 }
 
 int playgame() {
-
-    int gameover = 0; // Confirmar se o jogo acabou ou não
   
-    char uncovered_board[Side][Side]; // Regista o quadro de jogo com a localização de todas
+    char uncovered_board[SIDE][SIDE]; // Regista o quadro de jogo com a localização de todas
                                       // as bombas e números.
 
-    char current_board[Side][Side]; // Regista o quadro de jogo que deve ser
-                                    // mostrado ao utilizador.
+    char current_board[SIDE][SIDE];   // Regista o quadro de jogo que deve ser
+                                      // mostrado ao utilizador.
 
-    int totalmoves; // Regista o número total de movimentos que restam ao utilizador até ganhar o jogo.
+    int listbombs[NUMBOMBS][2];       // Regista (x,y), coordenadas para todas as bombas.
 
-    int i,j;
-    int x,y;
+    int gameover = 0; // Confirmar se o jogo acabou ou não
 
-    int listbombs[NUMBOMBS][2]; // Regista (x,y), coordenadas para todas as bombas.
-    
-    char opt;  // Opções de jogada.
+    int totalmoves;   // Regista o número total de movimentos que restam ao utilizador até ganhar o jogo.
 
-    srand(time(0)); // Implementa o gerador de números aleatórios.
+    int i, j, x, y;   // Variaveis de apoio
+    char opt;         // Opções de jogada.
+
+
+    // Inicializa o gerador de números aleatórios.
+    srand(time(0));
 
     // Inicializa o quadro de jogo.
-    for (i=0;i<Side;i++) {
-        for (j=0;j<Side;j++) {
+    for (i = 0; i < SIDE; i++) {
+        for (j = 0; j < SIDE; j++) {
             current_board[i][j] = 'X';
             uncovered_board[i][j] = 'X';
         }
@@ -67,10 +69,11 @@ int playgame() {
     getbombs(listbombs);
   
     // Coloca as bombas no quadro de jogo.
-    for (i=0;i<NUMBOMBS;i++) 
-        uncovered_board[listbombs[i][0]][listbombs[i][1]]='B';
+    for (i = 0; i < NUMBOMBS; i++) 
+        uncovered_board[listbombs[i][0]][listbombs[i][1]] = 'B';
   
-    totalmoves = Side*Side - NUMBOMBS;
+    // Inicializa variavel com o numero de jogadas restantes
+    totalmoves = SIDE * SIDE - NUMBOMBS;
 
     // Continuar a jogar até acabar o jogo.
     while (gameover == 0) {
@@ -79,8 +82,9 @@ int playgame() {
 
         // Escolhe tipo de jogada
         opt = getoption();
+
         // Obter um movimento do utilizador.
-        getmove(&x,&y);
+        getmove(&x, &y);
         
         if (opt == 'A' || opt == 'a') {
             // Executar esse movimento.
@@ -90,6 +94,10 @@ int playgame() {
             gameover = domark(current_board, listbombs, x, y);
         }
 
+        if (gameover >= 0 && totalmoves == 0) {
+            // Abriu todas as casas sem bombas - ganhou o jogo
+            gameover = 1;
+        }
         // Verificar se o utilizador ganhou ou perdeu.
         if (gameover == 1) {
             printf("Parabens, ganhou!!!\n");
@@ -102,94 +110,95 @@ int playgame() {
     }
 }
 
-//Pós-Instrução: Marcar a casa seleccionada pelo utilizador.
-
-
 //Pós-Instrução: Pedir ao utilizador o que deseja fazer na jogada,
 // 				 tem como opções, abrir a casa (A), ou marcar a casa (M).
 char getoption() {
 
     char opt;
-    int valid = 0;
+    int v = 0;
 
     do {
         printf("\nEscolha tipo de jogada: (A/M)  ");
 		scanf(" %c", &opt);
 
         if (opt == 'A' || opt == 'a' || opt == 'M' || opt == 'm')
-            valid = 1;
+            v = 1;
         else
             printf("Opção invalida! Por favor tente de novo\n");
-    } while (valid == 0);
+    } while (v == 0);
 
     return opt;
 }
 
 // Pós-Instrução: x e y vão obter os valores que estão escondidos
 // 				  nas coordenadas que o utilizador indicar.
-void getmove(int *x,int *y) {
+void getmove(int *x, int *y) {
 
     char c;
+    int v = 0;
 
-    // Ler o movimento
-    printf("A sua jogada:\nLinha: ");
-    scanf(" %c", &c);
-    printf("Coluna: ");
-    scanf("%d", y);
-    *x = (int)(c) - 65;
-    
-    // Ler novamente o movimento, caso o anterior tenha sido inválido
-    while (!valid(*x,*y)) {
-        printf("\nCoordenadas invalidas. Tente novamente.\n");
-		printf("A sua jogada:\nLinha: ");
+    do {
+        // Ler o movimento
+        printf("A sua jogada:\nLinha: ");
         scanf(" %c", &c);
-		printf("Coluna: ");
+        printf("Coluna: ");
         scanf("%d", y);
         *x = (int)(c) - 65;
-    }
+
+        v = valid(*x, *y);
+        if (v == 0) {
+            // Ler novamente o movimento, caso o anterior tenha sido inválido
+            printf("\nCoordenadas invalidas. Tente novamente.\n");
+        }
+    } while(v == 0);
 }
 
 // Pré-Instrução: A array de caracteres passados para a função
 //                devem ser um quadrado de array dimensional de tamanho SIZE
 // Pós-Instrução: O quadro de jogo apresentado na função será desenhado
 // 				  exactamente conforme os caracteres guardados na array.
-void printboard(char board[][Side]) {
+void printboard(char board[][SIDE]) {
 
-    int i,j,k,t;
+    int i, j, k, t;
 	char a=186, b=201, c=187, d=200, e=188, f=205;
 
     // Desenhar os lados da tabela
     printf("    ");
-    for (i=0;i<Side;i++)
+    for (i = 0; i < SIDE; i++)
         printf("%d   ", i);
 		
 	printf("\n  %c",b);
-	for (k=0;k<39;k++)
-		printf("%c",f);
-	printf("%c",c);
+	for (k = 0; k < 39; k++)
+		printf("%c", f);
+	printf("%c", c);
     printf("\n");
 
     // Desenhar o quadro de jogo.
-    for (i=0;i<Side;i++) {
+    for (i = 0; i < SIDE; i++) {
     
-        printf("%c %c", 65 + i , a);
-        for (j=0;j<Side;j++)
+        printf("%c %c", 65 + i, a);
+        for (j = 0; j < SIDE; j++)
             if (board[i][j] != '0')
-                printf(" %c %c",board[i][j], a);
+                printf(" %c %c", board[i][j], a);
             else
                 printf(" %c %c",' ', a);
         printf("\n");
 		
     }
-		printf("  %c",d);
-	for (k=0;k<39;k++)
-		printf("%c",f);
-	printf("%c",e);
+
+    printf("  %c",d);
+
+	for (k = 0; k < 39; k++)
+		printf("%c", f);
+
+	printf("%c", e);
     printf("\n");
-    printf("\nA - Abrir casa\t\t|\tM - Marcar/Desmarcar Bomba\nX - Casa por explorar\t|\t  - Casa vazia ja explorada\n\n");
+    printf("\nA - Abrir casa\t\t|\tM - Marcar/Desmarcar Bomba");
+    printf("\nX - Casa por explorar\t|\t  - Casa vazia ja explorada\n\n");
 }
 
-int domark(char board[][Side], int listbombs[][2], int row, int column) {
+//Pós-Instrução: Marcar a casa seleccionada pelo utilizador.
+int domark(char board[][SIDE], int listbombs[][2], int row, int column) {
 
     if (board[row][column] == 'M') {
         // desmarcar uma casa já marcada
@@ -226,52 +235,48 @@ int domark(char board[][Side], int listbombs[][2], int row, int column) {
 // Pós-Instrução: Esta função irá fazer com que o programa execute a jogada
 //                e fará as mudanças necessárias nas estruturas auxiliares 
 //                para assegurar que o jogo continua a correr devidamente.
-int domove(char board[][Side], char realboard[][Side],
+int domove(char board[][SIDE], char realboard[][SIDE],
             int listbombs[][2], int row, int column, int *totalmoves) {
 
     int i, j, num;
   
     // Tratar dos casos em que o utilizador perde o jogo.
-    if (realboard[row][column]=='B') {
+    if (realboard[row][column] == 'B') {
     
-        board[row][column]='B';
-        for (i=0;i<NUMBOMBS;i++) 
-            board[listbombs[i][0]][listbombs[i][1]]='B';
+        board[row][column] = 'B';
+        for (i = 0; i < NUMBOMBS; i++) 
+            board[listbombs[i][0]][listbombs[i][1]] = 'B';
     
         printboard(board);
         // Pisou uma mina - perdeu o jogo
         return -1;
     }
-
-    // Tratar dos casos em que o utilizador escolhe um espaço que já foi aberto ou marcado.
-    else if (board[row][column]!='X') {
-    
+    else if (board[row][column] != 'X') {
+        // Tratar dos casos em que o utilizador escolhe um espaço que já foi aberto ou marcado.
         printf("Essas coordenadas ja foram usadas, tente novamente!\n");
         return 0;
     }
-  
-    // Executar um movimento normal (Sem perder o jogo).
-    else {
- 
+    else {   
+        // Executar um movimento normal (Sem perder o jogo).
+    
         // Calcular o número de bombas adjacentes, e colocar este número
         // no quadro regular.
         num = numbombs(row, column, listbombs);
         (*totalmoves)--;
         
         // "Cast" para ter a certeza que o caractere é guardado na array
-        board[row][column]=(char)(num+'0');
+        board[row][column] = (char)(num + '0');
 
         // Se não houver bombas adjacentes, limpar a área em redor.
         if (num == 0) {
                 
             // Loop por todos os elementos adjacentes do quadrado actual.  
-            for (i=-1;i<2;i++) {
-	            for (j=-1;j<2;j++) {
-	  
+            for (i = -1; i < 2; i++) {
+	            for (j = -1; j < 2; j++) {
 	                // Limpar o quadrado apenas se ainda não foi
 	                // utilizado anteriormente.
-	                if (valid(row+i,column+j) && (board[row+i][column+j]=='X'))
-	                    domove(board, realboard, listbombs, row+i, column+j, totalmoves);
+	                if (valid(row + i, column + j) && (board[row + i][column + j] == 'X'))
+	                    domove(board, realboard, listbombs, row + i, column + j, totalmoves);
 	            }
             }
 	      
@@ -287,16 +292,16 @@ int domove(char board[][Side], char realboard[][Side],
 int valid(int row, int column) {
 
     // Procurar possiveis coordenadas inválidas.
-    if (row < 0) return 0;
-    
-    else if (row >= Side) return 0;
-  
-    else if (column < 0) return 0;
-  
-    else if (column >= Side) return 0;
-  
-    // If we get here we're good!!!!!!!!!!!!! \O/
-    else return 1;
+    if (row < 0 || column < 0) { 
+        return 0;
+    }
+    else if (row >= SIDE || column >= SIDE) { 
+        return 0;
+    }
+    else {
+        // If we get here we're good!!!!!!!!!!!!! \O/
+        return 1;
+    }
 }
 
 // Pré-Instrução: O tamanho da primeira dimensão de listbombs é NUMBOMBS,
@@ -307,12 +312,12 @@ int numbombs(int row ,int column ,int listbombs[][2]) {
 
     int i;
     int count = 0;
-    for (i=0;i<NUMBOMBS;i++) {
+    for (i = 0; i < NUMBOMBS; i++) {
 
         // Verificar bombas, uma a uma em cada quadrado para destapar.
-        if ((abs(row-listbombs[i][0])<=1+DUMMY) && (abs(column-listbombs[i][1])<=1+DUMMY)) {
-            count++; 
-        }  
+        if ((abs(row - listbombs[i][0]) <= 1 + DUMMY) && (abs(column - listbombs[i][1]) <= 1 + DUMMY)) {
+            count++;
+        }
     }
     
     return count;
@@ -324,25 +329,26 @@ int numbombs(int row ,int column ,int listbombs[][2]) {
 void getbombs(int bomblist[][2]) {
   
     int i;
+    int size = SIDE * SIDE;
     int Hdummy;
-    int curbombs[Side*Side];
+    int curbombs[size];
   
-    for (i=0; i<Side*Side; i++)
+    for (i = 0; i < size; i++)
         curbombs[i] = 0;
     
     // Controla o número de bombas colocadas actualmente no jogo.
     i = 0;
  
     // Continuar até que todas as bombas aleatórias sejam criadas.
-    while(i<NUMBOMBS) {
+    while(i < NUMBOMBS) {
     
-        Hdummy = rand()%(Side*Side);
+        Hdummy = rand() % (size);
 
         // Adicionar bomba apenas se não for em duplicado.
         if (curbombs[Hdummy]==0) {
       
-            bomblist[i][0] = Hdummy/Side;
-            bomblist[i][1] = Hdummy%Side;
+            bomblist[i][0] = Hdummy / SIDE;
+            bomblist[i][1] = Hdummy % SIDE;
             i++;
             curbombs[Hdummy] = 1;
         }
